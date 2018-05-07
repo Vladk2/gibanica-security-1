daa524398f79b5a992fe89c786b94db1bf3fdd9a
stefan <stefannn.live@live.com>
2018-05-07 17:35:04 +0200

    cert added/

--- gibanica/client/app/bundles/Logs/components/LogsListing.jsx
+++ gibanica/client/app/bundles/Logs/components/LogsListing.jsx
@@ -16,10 +16,20 @@ export default class LogsListing extends React.Component {
     render() {
         const { logs } = this.state;
         return (
-            <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        A simple primary alert—check it out!
+                    </div>
+                    <div className="col-md-6">
+                        <button type="button" className="btn btn-primary">
+                          Notifications <span className="badge badge-light">4</span>
+                        </button>
+                    </div>
+                </div>
                 {
                     logs.map(log => {
-                        console.log(log);
+                        //console.log(log);
                     })
                 }
             </div>
