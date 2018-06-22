4a7a7dd082efd55aacc01641dcf8288a894131b6
stefan <stefannn.live@live.com>
2018-06-22 17:08:38 +0200

    color for carousel selection changed to gray. alarmbox moved to separate component

--- gibanica/client/src/components/AlarmsListing.jsx
+++ gibanica/client/src/components/AlarmsListing.jsx
@@ -1,15 +1,6 @@
 import React from "react";
 
-import LinkBottomIcon from "grommet/components/icons/base/LinkBottom";
-import LinkTopIcon from "grommet/components/icons/base/LinkTop";
-import AlertIcon from "grommet/components/icons/base/Alert";
-import Carousel from "grommet/components/Carousel";
-import Meter from "grommet/components/Meter";
-import Box from "grommet/components/Box";
-import Value from "grommet/components/Value";
-
-import { Collapse } from "react-collapse";
-
+import AlarmBox from "./common/AlarmBox";
 import NavBar from "./navbar/NavBar";
 
 export default class AlarmsListing extends React.Component {
@@ -47,102 +38,12 @@ export default class AlarmsListing extends React.Component {
         <NavBar />
         <br />
         {alarms.map((a, i) => (
-          <div
-            className="row"
+          <AlarmBox
             key={i}
-            style={{ textAlign: "center", verticalAlign: "middle" }}
-          >
-            <div className="col-md-12">
-              <div className="row">
-                <div
-                  className="col-md-9"
-                  style={{
-                    borderLeftWidth: 0.5,
-                    borderLeftColor: "#CFCFCF",
-                    borderLeftStyle: "solid",
-                    backgroundColor: "#FFFFFF",
-                    height: window.innerHeight / 7
-                  }}
-                >
-                  <div
-                    className="col-md-3"
-                    style={{
-                      position: "relative",
-                      top: "50%",
-                      transform: "translateY(-50%)"
-                    }}
-                  >
-                    <AlertIcon colorIndex="critical" size="large" />
-                  </div>
-                  <div
-                    className="col-md-9"
-                    style={{
-                      position: "relative",
-                      top: "50%",
-                      transform: "translateY(-50%)"
-                    }}
-                  >
-                    <p>Alarm {a.alarm}</p>
-                  </div>
-                </div>
-                <div
-                  className="col-md-3"
-                  style={{
-                    margin: "auto",
-                    borderLeftWidth: 0.5,
-                    borderLeftColor: "#CFCFCF",
-                    borderLeftStyle: "solid",
-                    borderRightWidth: 0.5,
-                    borderRightColor: "#CFCFCF",
-                    borderRightStyle: "solid",
-                    backgroundColor: "#FFFFFF",
-                    height: window.innerHeight / 7
-                  }}
-                >
-                  <div
-                    style={{
-                      position: "relative",
-                      top: "50%",
-                      transform: "translateY(-50%)"
-                    }}
-                  >
-                    <div>
-                      <Box>
-                        <Value value={1000} units="logs" align="start" />
-                        <Meter vertical={false} value={40} />
-                      </Box>
-                    </div>
-                    <div>
-                      <i>Latest alarm logs</i>
-                    </div>
-                    <div>
-                      {a.collapsed ? (
-                        <LinkTopIcon
-                          onClick={() => this.toggleExpandAlarm(i)}
-                          colorIndex="neutral-4"
-                        />
-                      ) : (
-                        <LinkBottomIcon
-                          onClick={() => this.toggleExpandAlarm(i)}
-                          colorIndex="neutral-4"
-                        />
-                      )}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <Collapse isOpened={a.collapsed}>
-                <Carousel style={{ height: window.innerHeight / 4 }}>
-                  <p>Log 1 from Alarm {a.alarm}</p>
-                  <p>Log 2 from Alarm {a.alarm}</p>
-                  <p>Log 3 from Alarm {a.alarm}</p>
-                  <p>Log 4 from Alarm {a.alarm}</p>
-                  <p>Log 5 from Alarm {a.alarm}</p>
-                </Carousel>
-              </Collapse>
-              <br />
-            </div>
-          </div>
+            index={i}
+            alarm={a}
+            toggleExpandAlarm={this.toggleExpandAlarm}
+          />
         ))}
       </div>
     );
---
+++ gibanica/client/src/components/common/AlarmBox.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+
+import CaretDownIcon from "grommet/components/icons/base/CaretDown";
+import CaretUpIcon from "grommet/components/icons/base/CaretUp";
+import AlertIcon from "grommet/components/icons/base/Alert";
+import Carousel from "grommet/components/Carousel";
+import Meter from "grommet/components/Meter";
+import Box from "grommet/components/Box";
+import Value from "grommet/components/Value";
+import Label from "grommet/components/Label";
+
+import { Collapse } from "react-collapse";
+
+export default class AlarmBox extends React.Component {
+  render() {
+    const { alarm, index, toggleExpandAlarm } = this.props;
+    return (
+      <div
+        className="row"
+        style={{ textAlign: "center", verticalAlign: "middle" }}
+      >
+        <div className="col-md-12">
+          <div className="row">
+            <div
+              className="col-md-9"
+              style={{
+                borderLeftWidth: 0.5,
+                borderLeftColor: "#CFCFCF",
+                borderLeftStyle: "solid",
+                backgroundColor: "#FFFFFF",
+                height: window.innerHeight / 7
+              }}
+            >
+              <div
+                className="col-md-3"
+                style={{
+                  position: "relative",
+                  top: "50%",
+                  transform: "translateY(-50%)"
+                }}
+              >
+                <AlertIcon colorIndex="critical" size="large" />
+              </div>
+              <div
+                className="col-md-9"
+                style={{
+                  position: "relative",
+                  top: "50%",
+                  transform: "translateY(-50%)"
+                }}
+              >
+                <p>Alarm {alarm.alarm}</p>
+              </div>
+            </div>
+            <div
+              className="col-md-3"
+              style={{
+                margin: "auto",
+                borderLeftWidth: 0.5,
+                borderLeftColor: "#CFCFCF",
+                borderLeftStyle: "solid",
+                borderRightWidth: 0.5,
+                borderRightColor: "#CFCFCF",
+                borderRightStyle: "solid",
+                backgroundColor: "#FFFFFF",
+                height: window.innerHeight / 7
+              }}
+            >
+              <div
+                style={{
+                  position: "relative",
+                  top: "50%",
+                  transform: "translateY(-50%)"
+                }}
+              >
+                <div>
+                  <Box align="center">
+                    <Meter size="xsmall" type="arc" value={40} max={1000} />
+                    <Box
+                      direction="row"
+                      justify="between"
+                      align="center"
+                      pad={{ between: "small" }}
+                      responsive={false}
+                    >
+                      <Label size="small">0</Label>
+                      <Value value={40} units="logs" size="xsmall" />
+                      <Label size="small">1000</Label>
+                    </Box>
+                  </Box>
+                </div>
+                <div style={{ paddingTop: 10 }}>
+                  {alarm.collapsed ? (
+                    <CaretUpIcon
+                      onClick={() => toggleExpandAlarm(index)}
+                      colorIndex="neutral-4"
+                    />
+                  ) : (
+                    <CaretDownIcon
+                      onClick={() => toggleExpandAlarm(index)}
+                      colorIndex="neutral-4"
+                    />
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+          <Collapse isOpened={alarm.collapsed}>
+            <Carousel style={{ height: window.innerHeight / 4 }}>
+              <p>Log 1 from Alarm {alarm.alarm}</p>
+              <p>Log 2 from Alarm {alarm.alarm}</p>
+              <p>Log 3 from Alarm {alarm.alarm}</p>
+              <p>Log 4 from Alarm {alarm.alarm}</p>
+              <p>Log 5 from Alarm {alarm.alarm}</p>
+            </Carousel>
+          </Collapse>
+          <br />
+        </div>
+      </div>
+    );
+  }
+}
